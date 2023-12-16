240f563cba3fd88e890f5c875df516855434ab15
MarioMH8 <mhidalgomario@gmail.com>
2023-12-16 01:52:00 +0100

    fix: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
 	darkMode: 'media',
-	content: ['./src/**/*.{js,ts,jsx,tsx,mdx,astro}', '../node_modules/@mariomh/**/*.{js,ts,jsx,tsx,mdx,astro}'],
+	content: ['./src/**/*.{js,ts,jsx,tsx,mdx,astro}'],
 	theme: {
 		extend: {
 			// Tailwind css extensions
