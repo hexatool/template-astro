af8173a97aa4c094712aca216422d869b0696295
MarioMH8 <mhidalgomario@gmail.com>
2023-12-19 02:26:28 +0100

    feat: add preact components

---
+++ src/features/title.tsx
@@ -0,0 +1,18 @@
+import { useStore } from '@nanostores/preact';
+import { type JSX } from 'preact';
+import { i18n } from '../state/locale';
+
+
+const messages = i18n('hello', {
+	hello: 'Hello world',
+});
+
+const Title = (): JSX.Element => {
+	const t = useStore(messages);
+
+	return (
+		<h1 className='text-2xl text-center'>{t.hello} 👋🏼</h1>
+	);
+};
+
+export default Title;
